9e383a21b50d07d9a3c6756d3a882668eb5a3f0d
Sarathkrishnan_internship <sarathkrishnangs@gmail.com>
2023-05-25 13:34:36 +0530

    bias and weights

--- l4_model_layer_implimentation_in_c/read_npy/read_npy.py
+++ l4_model_layer_implimentation_in_c/read_npy/read_npy.py
@@ -1,6 +1,7 @@
 import numpy as np
 from os import listdir
 
+
 file_path =r'C:\Users\Trest\Desktop\SarathKrishnan\codes\Netrasemi-ML-Internship\l4_model_layer_implimentation_in_c\read_npy\weights\7_dense_1.npy'
 x = np.load(file_path)
 print(x.shape)
